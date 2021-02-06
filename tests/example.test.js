00e58a00f32789e5c0e61341a57105482c814e9c
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-05 22:39:21 -0300

    16. Interacting with Dropdown

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,11 +2,16 @@ const puppeteer = require('puppeteer')
 
 describe('My first puppeteer test', () => {
   it('should launch the browser', async () => {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 100 })
+    const browser = await puppeteer.launch({ headless: false, slowMo: 10 })
     const page = await browser.newPage()
+    const name = 'shimira'
     await page.goto('https://devexpress.github.io/testcafe/example/')
-    await page.type('#developer-name', 'shimira', { delay: 0 })
+    await page.type('#developer-name', name, { delay: 0 })
     await page.click('#tried-test-cafe', { clickCount: 1 })
+    await page.select('#preferred-interface', 'JavaScript API')
+    await page.type('#comments', 'I am a machine')
+    await page.click('#submit-button')
+    await page.waitForSelector('h1')
     await page.waitForTimeout(2000)
 
     await browser.close()
