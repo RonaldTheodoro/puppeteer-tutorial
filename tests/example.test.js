2c426bdf610b64d7fc2eb509c0f88f299685a3d5
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-05 22:42:42 -0300

    17. Get Page Title & URL

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,16 +4,12 @@ describe('My first puppeteer test', () => {
   it('should launch the browser', async () => {
     const browser = await puppeteer.launch({ headless: false, slowMo: 10 })
     const page = await browser.newPage()
-    const name = 'shimira'
-    await page.goto('https://devexpress.github.io/testcafe/example/')
-    await page.type('#developer-name', name, { delay: 0 })
-    await page.click('#tried-test-cafe', { clickCount: 1 })
-    await page.select('#preferred-interface', 'JavaScript API')
-    await page.type('#comments', 'I am a machine')
-    await page.click('#submit-button')
-    await page.waitForSelector('h1')
-    await page.waitForTimeout(2000)
+    await page.goto('http://example.com/')
+    const title = await page.title()
+    const url = await page.url()
 
+    console.log(`title: ${title}`)
+    console.log(`url: ${url}`)
     await browser.close()
   })
 })
