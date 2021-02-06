fcca0b5e02143365c2c14776165f008f0fa8aa0d
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-05 22:58:51 -0300

    20. Assertions

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,15 +1,20 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
 describe('My first puppeteer test', () => {
   it('should launch the browser', async () => {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 10 })
+    const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto('http://example.com/')
+    const title = await page.title()
+    const url = await page.url()
     const text = await page.$eval('h1', (element) => element.textContent)
     const count = await page.$$eval('p', (element) => element.length)
 
-    console.log(`text: ${text}`)
-    console.log(`count: ${count}`)
+    expect(title).to.be.a('string', 'Example Domain')
+    expect(url).to.include('example.com')
+    expect(text).to.be.a('string', 'Example Domain')
+    expect(count).to.equal(2)
 
     await browser.close()
   })
