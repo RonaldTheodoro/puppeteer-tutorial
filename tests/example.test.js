da974bd88aaab0058ceeea50f8be82143985af90
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-05 22:26:57 -0300

    14. Interacting with Inputs

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,14 +4,9 @@ describe('My first puppeteer test', () => {
   it('should launch the browser', async () => {
     const browser = await puppeteer.launch({ headless: false, slowMo: 100 })
     const page = await browser.newPage()
-    await page.goto('http://example.com/')
-    await page.waitForSelector('h1')
-    await page.goto('https://dev.to')
-    await page.waitForSelector('#body-styles')
-    await page.goBack()
-    await page.waitForSelector('h1')
-    await page.goForward()
-    await page.waitForSelector('#body-styles')
+    await page.goto('https://devexpress.github.io/testcafe/example/')
+    await page.type('#developer-name', 'shimira', { delay: 0 })
+    await page.waitForTimeout(5000)
 
     await browser.close()
   })
