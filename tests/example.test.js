29d7d56da2641e27b31603cdc0b35e6dbac48740
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-05 22:18:52 -0300

    13. Go Back & Forward in the Browser

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,14 +2,16 @@ const puppeteer = require('puppeteer')
 
 describe('My first puppeteer test', () => {
   it('should launch the browser', async () => {
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: false, slowMo: 100 })
     const page = await browser.newPage()
     await page.goto('http://example.com/')
-    await page.waitForTimeout(3000)
     await page.waitForSelector('h1')
-    await page.reload()
-    await page.waitForTimeout(3000)
+    await page.goto('https://dev.to')
+    await page.waitForSelector('#body-styles')
+    await page.goBack()
     await page.waitForSelector('h1')
+    await page.goForward()
+    await page.waitForSelector('#body-styles')
 
     await browser.close()
   })
