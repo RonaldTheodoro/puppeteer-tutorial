18317a9ff81a106a991e1efc0a434acff0182a07
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2021-02-05 22:49:29 -0300

    19. Get Element Count

--- tests/example.test.js
+++ tests/example.test.js
@@ -6,8 +6,10 @@ describe('My first puppeteer test', () => {
     const page = await browser.newPage()
     await page.goto('http://example.com/')
     const text = await page.$eval('h1', (element) => element.textContent)
+    const count = await page.$$eval('p', (element) => element.length)
 
     console.log(`text: ${text}`)
+    console.log(`count: ${count}`)
 
     await browser.close()
   })
